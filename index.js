2387b658c69b00756e07bc70f007030cbab04457
didierganthier <didierganthierperan@gmail.com>
2020-11-21 08:27:05 -0500

    utility variables created

---
+++ index.js
@@ -0,0 +1,16 @@
+//Based on Alex Tusinean Flappy Bird Clone
+
+var $ = (id) => document.getElementById(id);
+
+var gravity;
+var advance;
+
+var game_score = 0;
+
+var gravityPoints = 0;
+
+var alive = true;
+
+const flySpeed = 9;
+
+
