e1a2c1e0812667fc98ecbd15bdda2d05835944d4
didierganthier <didierganthierperan@gmail.com>
2020-11-21 08:40:18 -0500

    fly method created

--- index.js
+++ index.js
@@ -7,10 +7,27 @@ var advance;
 
 var game_score = 0;
 
-var gravityPoints = 0;
+var gravityPoint = 0;
 
 var alive = true;
 
 const flySpeed = 9;
 
+function fly(){
+    if(alive == true){
+        $('player').style.transition = "all 0.5s";
+        $("player").style.top = $("player").offsetTop - window.innerHeight/10 - 2 + 'px';
+        clearInterval(gravity);
+        gravity = false;
 
+        $("player").style.transform = "rotate(-20deg)";
+        gravityPoint = 0;
+        setTimeout(function(){
+            if(gravity === false){
+                gravity = setInterval(fall, 15);
+                $("game").addEventListener("click", fly, {once : true});
+                $("player").style.transition = "";
+            }
+        }, 500)
+    }
+}
